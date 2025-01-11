afd0631b57f3b5c8aeea9840fadf45489c3b6b59
dionisioR <72356766+dionisioR@users.noreply.github.com>
2025-01-11 13:14:17 -0300

    Update 2025-01-11   13h14
    Update 2025-01-11   13h14

---
+++ LancheMac_MsSQL/LancheMac_MsSQL/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace LancheMac_MsSQL.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
